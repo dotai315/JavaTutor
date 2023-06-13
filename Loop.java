6ad70f909459244b52adc2dce35bac42bbaf7755
dotai315 <dotai.3373@gmail.com>
2023-06-13 23:13:52 +0700

    Loop Demo

---
+++ Loop.java
@@ -0,0 +1,15 @@
+import java.io.*;
+
+class Loop {
+	public static void main(String[] args) {
+		int arr[] = {5, 4, 3, 2, 1};
+
+		for(int i = 0; i < 10; i++) {
+			System.out.println("index: " + i);
+		}
+		
+		for(int i : arr) {
+			System.out.println("value in arr" + i);
+		}
+	}
+}
